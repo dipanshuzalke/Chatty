5c857e99478dbd3ec745b26969f4668904c5e1a9
Dipanshu Zalke <dipanshuzalke@gmail.com>
2025-08-07 01:51:12 +0530

    #

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -5,9 +5,7 @@ export const connectDB = async () => {
     if (!process.env.MONGODB_URI) {
       throw new Error("MONGODB_URI is not defined in environment variables");
     }
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("MongoDB connection error:", error.message);
